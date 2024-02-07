533a21ccaca3dc5ca89f4333fbf963fa82472f82
Zach Oakes <zoakes1@gmail.com>
2024-02-06 18:30:59 -0700

    Add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthenticationService} from "./authentication.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '30-by-30-step10';
+    title = 'Flight Tracker';
+
+    constructor(
+        private router: Router,
+        public authService: AuthenticationService) {
+    }
+
+    logout(): void {
+        this.authService.setIsAuthenticated(false);
+        sessionStorage.setItem('username', '');
+        this.router.navigate(['/login'])
+            .then(() => {
+                location.reload();
+            });
+    }
 }
